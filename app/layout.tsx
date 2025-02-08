c249706c01554e24d8be482f5325f109542c8b67
Ian Keller <ianwkeller@gmail.com>
2025-02-07 17:07:04 -0700

    fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,9 @@ export default function RootLayout({
             <main className="relative">
               {!isHome ? (
                 <div className="absolute inset-0 max-w-7xl mx-auto">
-                  <div className="h-full min-h-screen bg-zinc-900/80 backdrop-blur-sm rounded-lg" />
+                  <div className="h-full min-h-screen bg-white dark:bg-zinc-900 backdrop-blur-sm rounded-lg p-8">
+                    {children}
+                  </div>
                 </div>
               ) : (
                 <div className="flex flex-col items-center justify-center min-h-screen">
